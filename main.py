c39487af3724a21a43f3cebe997d1eb2b49e28cd
Godwin Dixon <156458878+gwinski@users.noreply.github.com>
2024-04-09 13:38:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# user_input.py
+
+# Ask user for their name
+name = input("What is your name? ")
+
+# Ask user for their age
+age = input("How old are you? ")
+
+# Ask user for their location
+location = input("Where are you located? ")
+
+# Print personalized message
+print("Hello {}, you are {} years old and live in {}.".format(name, age, location))
